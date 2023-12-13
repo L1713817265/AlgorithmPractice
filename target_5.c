eeea07e577587907f8e0465fc4faaae96967988c
L1713817265 <1713817265@qq.com>
2023-12-13 10:42:36 +0800

    [新增] 新增target_5.c

--- target_5.c
+++ target_5.c
@@ -9,6 +9,7 @@ int main()
 {
     int arraylen = 0;
     int count = 0;
+    int count1 = 0;
 
     printf("请输入数组nums的长度:\n");
     scanf("%d", &arraylen);
@@ -27,6 +28,7 @@ int main()
     }
     printf("\n");
 
+#if 0
     for(int idx = 0; idx < (arraylen - 1); idx++)
     {
         for(int idx1 = (idx + 1); idx1 < (arraylen - count); idx1++)
@@ -41,9 +43,27 @@ int main()
             }
         }
     }
+#endif
+
+    for(int idx = 0; idx < (arraylen - count1 - 1); idx++)
+    {
+        int position = idx + 1;
+        for(int idx1 = idx + 1; idx1 < (arraylen - count1); idx1++)
+        {
+            if(nums[idx1] != nums[idx])
+            {
+                nums[position++] = nums[idx1];
+            }
+            else
+            {
+                count++;
+            }
+        }
+        count1 = count;
+    }
 
     printf("数组为：       ");
-    for(int idx = 0; idx < (arraylen - count); idx++)
+    for(int idx = 0; idx < (arraylen - count1); idx++)
     {
         printf(" %d", nums[idx]);
     }
